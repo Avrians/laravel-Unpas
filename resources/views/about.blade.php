585879103491a18316c3efba00d7f2efe6391f00
avrians <avriansyahb10@gmail.com>
2023-04-07 15:00:10 +0700

    update tutor 3 20.10

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,9 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
     <title>Avrians Blog | About</title>
 </head>
 <body>
     <h1>Halaman About</h1>
+    <h3>Avriansyah Bahtiar</h3>
+    <p>avriansyah@gmail.com</p>
+    <img src="img/kura.jpg" alt="kura">
 </body>
 </html>
