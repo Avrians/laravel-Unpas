dabde6b4868db5a2c9bcec8c47ed2e9d5679953d
avrians <avriansyahb10@gmail.com>
2023-04-08 15:52:06 +0700

    update tutor 5 18.35

---
+++ app/Models/Post.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+
+
+class Post
+{
+    private static $blog_posts = [
+        [
+            "title" => "Judul Post Pertama", 
+            "slug" => "judul-post-pertama",
+            "author" => "Avriansyah Bahtiar",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt dolore quam autem assumenda quaerat similique blanditiis architecto in, tempore inventore doloremque error provident dolorum dicta dolores odio quia atque eos sapiente impedit cupiditate iste expedita eaque! Consectetur nisi vitae, dicta totam nulla eveniet dignissimos explicabo reprehenderit porro! Maxime provident a quae iste assumenda, quisquam fugiat unde, qui est incidunt consectetur culpa molestias ratione eligendi debitis neque, placeat illo voluptatum soluta cum. Similique cupiditate reprehenderit quia ipsum temporibus totam voluptatibus pariatur?"
+        ],
+        [
+            "title" => "Judul Post Kedua", 
+            "slug" => "judul-post-kedua",
+            "author" => "Avriansyah",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt dolore quam autem assumenda quaerat similique blanditiis architecto in, tempore inventore doloremque error provident dolorum dicta dolores odio quia atque eos sapiente impedit cupiditate iste expedita eaque! Consectetur nisi vitae, dicta totam nulla eveniet dignissimos explicabo reprehenderit porro! Maxime provident a quae iste assumenda, quisquam fugiat unde, qui est incidunt consectetur culpa molestias ratione eligendi debitis neque, placeat illo voluptatum soluta cum. Similique cupiditate reprehenderit quia ipsum temporibus totam voluptatibus pariatur?"
+        ]
+    ];
+
+    public static function all() {
+        return self::$blog_posts;
+    }
+
+    public static function find($slug) {
+        $posts  = self::$blog_posts;
+        $post = [];
+
+        foreach($posts as $p) {
+            if($p["slug"] == $slug) {
+                $post = $p;
+            }
+        }
+
+        return $post;
+        
+    }
+}
