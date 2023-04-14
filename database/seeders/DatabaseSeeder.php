c1e559a5f6ff07a90b4c0881868948d7c267aff9
avrians <avriansyahb10@gmail.com>
2023-04-14 15:58:50 +0700

    finish tutor 9

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -31,6 +31,12 @@ class DatabaseSeeder extends Seeder
             'password' => bcrypt('12345')
         ]);
 
+        User::create([
+            'name' => 'Bahtiar',
+            'email' => 'bahtiar@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
         Category::create(
             [
             'name' => 'Web Programming',
@@ -58,12 +64,20 @@ class DatabaseSeeder extends Seeder
                     'user_id' => 1 ]);
 
             Post::create([
-                'title' => 'Judul Ke Tida',
+                'title' => 'Judul Ke Tiga',
                 'slug' => 'judul-ke-tiga',
                 'excerpt' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur dolore, quam odio accusamus debitis impedit quae, asperiores inventore?',
                 'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur dolore, quam odio accusamus debitis impedit quae, dicta repellendus sit quaerat, sequi necessitatibus asperiores inventore? Aliquid amet non excepturi unde! Blanditiis id deserunt numquam laudantium impedit consectetur, qui mollitia officia aliquid consequuntur ea repellat magni nihil quos enim autem maiores deleniti fugit facere doloribus adipisci repudiandae veniam nulla! Vitae earum ut eum vel minus. Culpa mollitia reprehenderit ut eaque, possimus quam voluptates quibusdam at commodi quod provident animi officia eligendi molestiae dicta quasi ad assumenda. Qui ducimus enim optio debitis. Et voluptatem aliquam sit quos maxime corporis voluptas, magni id nemo.',
                 'category_id' => 1,
                 'user_id' => 1]);
+
+            Post::create([
+                    'title' => 'Judul Ke Empat',
+                    'slug' => 'judul-ke-empat',
+                    'excerpt' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur dolore, quam odio accusamus debitis impedit quae, asperiores inventore?',
+                    'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur dolore, quam odio accusamus debitis impedit quae, dicta repellendus sit quaerat, sequi necessitatibus asperiores inventore? Aliquid amet non excepturi unde! Blanditiis id deserunt numquam laudantium impedit consectetur, qui mollitia officia aliquid consequuntur ea repellat magni nihil quos enim autem maiores deleniti fugit facere doloribus adipisci repudiandae veniam nulla! Vitae earum ut eum vel minus. Culpa mollitia reprehenderit ut eaque, possimus quam voluptates quibusdam at commodi quod provident animi officia eligendi molestiae dicta quasi ad assumenda. Qui ducimus enim optio debitis. Et voluptatem aliquam sit quos maxime corporis voluptas, magni id nemo.',
+                    'category_id' => 2,
+                    'user_id' => 2]);
 
 
     }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,10 +4,10 @@
     <article>
         <h2> {{ $post->title }}</h2>
 
-        <p>By. Avriansyah in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+        <p>By. <a href="#"  class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
 
         {{-- kurung ini berfungsi untuk menjalankan semua perintah html walaupun ada di text jika memakai kurung biasa tidak bisa --}}
         {!! $post->body !!}
  
-    <a href="/posts">Back to Post</a>
+    <a href="/posts" class="d-block mt-3">Back to Post</a>
 @endsection
