7b82d3acf6a134c064dae9f5350c50e1911ff3af
avrians <avriansyahb10@gmail.com>
2023-04-15 16:47:24 +0700

    update tutor 12 12.15

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -14,6 +14,7 @@ class Post extends Model
 
     // ini yang ngga boleh, sisa ny boleh
     protected $guarded = ['id'];
+    protected $with = ['category', 'author'];
 
     public function category() {
         return $this->belongsTo(Category::class);
