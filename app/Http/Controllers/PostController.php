495c1d387afc450ac1ef7c76377c12fa4c2a0199
avrians <avriansyahb10@gmail.com>
2023-04-16 23:17:14 +0700

    update tutor 12 07.05

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     public function index(){
+        dd(request(('search')));
         return view('posts', [
             "title" => "All Posts",
             "active" => 'posts',
