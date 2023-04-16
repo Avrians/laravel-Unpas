56ad5fe55fa38f72f91ef123a0c597b18ba84ac0
avrians <avriansyahb10@gmail.com>
2023-04-16 15:42:57 +0700

    finish tutor 12

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,13 +10,15 @@ class PostController extends Controller
     public function index(){
         return view('posts', [
             "title" => "All Posts",
+            "active" => 'posts',
             "posts" => Post::latest()->get()
         ]);
     }
 
     public function show(Post $post) {
         return view('post', [
-            "title" => "Single Post", 
+            "title" => "Single Post",
+            "active" => 'posts',
             "post" => $post
         ]);
     }
