c76bddcacb8e74144c4c5eb17b852dadf549dd92
avrians <avriansyahb10@gmail.com>
2023-04-20 13:09:36 +0700

    update tutor 13 41.50

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -26,7 +26,7 @@
                 <a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none text-dark">{{ $posts[0]->title }}</a>
             </h3>
             <p><small class="text-body-secondary"> 
-                By. <a href="/authors/{{ $posts[0]->author->username }}"  class="text-decoration-none">{{ $posts[0]->author->name }}</a> in <a href="/posts?category={{ $posts[0]->category->slug }}"class="text-decoration-none">{{ $posts[0]->category->name }}</a> {{ $posts[0]->created_at->diffForHumans() }}
+                By. <a href="/posts?authors={{ $posts[0]->author->username }}"  class="text-decoration-none">{{ $posts[0]->author->name }}</a> in <a href="/posts?category={{ $posts[0]->category->slug }}"class="text-decoration-none">{{ $posts[0]->category->name }}</a> {{ $posts[0]->created_at->diffForHumans() }}
             </small></p>
 
             <p class="card-text">{{ $posts[0]->excerpt }}</p>
@@ -47,7 +47,7 @@
                     <div class="card-body">
                       <h5 class="card-title">{{ $post->title }}</h5>
                       <p><small class="text-body-secondary"> 
-                        By. <a href="/authors/{{ $post->author->username }}"  class="text-decoration-none">{{ $post->author->name }}</a>  {{ $post->created_at->diffForHumans() }}
+                        By. <a href="/posts?authors={{ $post->author->username }}"  class="text-decoration-none">{{ $post->author->name }}</a>  {{ $post->created_at->diffForHumans() }}
                         </small></p>
 
                       <p class="card-text">{{ $post->excerpt }}</p>
