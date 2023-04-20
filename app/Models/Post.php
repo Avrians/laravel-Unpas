41cad4a743933e0de7d07ab618f1816c699bab9e
avrians <avriansyahb10@gmail.com>
2023-04-20 13:02:14 +0700

    update tutor 13 39.50

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -30,11 +30,17 @@ class Post extends Model
             ->orWhere('body','like','%'.$search.'%');
         });
 
+        // versi callback
         $query->when($filters['category'] ?? false, function($query, $category){
             return $query->whereHas('category', function($query) use ($category){
                 $query->where('slug', $category);
             });
         });
+
+        // versi hero function
+        $query->when($filters['author'] ?? false, fn($query, $author) =>
+            $query->whereHas('author', fn($query)=>
+                $query->where('username', $author)) )
     }
 
     public function category() {
