3ef0e71f0bb7e5ed2e5ccf96cbbf723b2e0ea6c0
avrians <avriansyahb10@gmail.com>
2023-04-21 20:36:14 +0700

    finish tutor 4 Membuat view registrasi

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -3,7 +3,7 @@
 @section('container')
 
 <div class="row justify-content-center">
-  <div class="col-md-4">
+  <div class="col-md-5">
     <main class="form-signin w-100 m-auto">
       <h1 class="h3 mb-3 fw-normal text-center">Please Login</h1>
         
