81602af0b01913985d3e8164513e798e150a5c62
avrians <avriansyahb10@gmail.com>
2023-04-23 12:40:45 +0700

    update tutor 16 16.30

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -12,14 +12,26 @@
     </div>
     @endif
 
+    @if(session()->has('loginEror'))
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+      {{ session('loginEror') }}
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    @endif
+
     <main class="form-signin w-100 m-auto">
       <h1 class="h3 mb-3 fw-normal text-center">Please Login</h1>
         
         <form action="/login" method="POST">
           @csrf
           <div class="form-floating">
-            <input type="email" name="email" class="form-control" id="email" placeholder="name@example.com" autofocus required>
+            <input type="email" name="email" class="form-control @error('email') is-invalid @enderror)" id="email" placeholder="name@example.com" value="{{ old('email') }}" autofocus required>
             <label for="email">Email address</label>
+            @error('email')
+              <div class="div invalid-feedback">
+                {{ $message }}
+              </div>
+            @enderror
           </div>
           <div class="form-floating">
             <input type="password" name="password" class="form-control" id="floatingPassword" placeholder="Password" required>
